0e5c8922cb307695255414b6d2d7559373346313
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2026-02-12 15:37:39 -0400

    Added Classes
    No functionality yet

--- Program.cs
+++ Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace RPGPlayable
 {
+    class Health
+    {
+        //
+    }
+
+    class Entity
+    {
+        //
+    }
+
+    class Player : Entity
+    {
+        //
+    }
+
+    class Enemy : Entity
+    {
+        //
+    }
+
+
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            //
         }
     }
 }
