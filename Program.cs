fd1215d95425976ee6dd5ada9cebcba125ac950d
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2026-02-12 16:20:52 -0400

    Started Class Functionality for; Health, Entity, Player & Enemy

--- Program.cs
+++ Program.cs
@@ -9,22 +9,65 @@ namespace RPGPlayable
 {
     class Health
     {
-        //
+        public int Current { get; private set; }
+        public int Max { get; private set; }
+
+        public bool IsDead => Current <= 0;
+
+        public Health(int max)
+        {
+            Max = max;
+            Current = max;
+        }
+
+        public void TakeDamage(int amount)
+        {
+            Current -= amount;
+            if (Current < 0) Current = 0;
+        }
     }
 
-    class Entity
+    abstract class Entity
     {
-        //
+        public int X { get; protected set; }
+        public int Y { get; protected set; }
+        public char Mark { get; protected set; }
+        public Health Health { get; private set; }
+
+        protected Entity(int x, int y, char symbol, int hp)
+        {
+            X = x;
+            Y = y;
+            Mark = mark;
+            Health = new Health(hp);
+        }
+
+        public void SetPosition(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Attack(Entity target)
+        {
+            target.Health.TakeDamage(1);
+        }
     }
 
     class Player : Entity
     {
-        //
+        public Player(int x, int y) : base(x, y, 'P', 5)
+        {
+
+        }
     }
 
     class Enemy : Entity
     {
-        //
+        public Enemy(int x, int y) : base(x, y, 'P', 5)
+        {
+            
+        }
     }
 
     class Map
